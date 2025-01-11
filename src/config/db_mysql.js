204c29b154ae2fa6edf25a54adbffdcfa9705276
beesaiyvoud <laoseasybee@gmail.com>
2025-01-11 13:09:26 +0700

    add

--- src/config/db_mysql.js
+++ src/config/db_mysql.js
@@ -1,17 +1,17 @@
 import mysql from "mysql";
-// const connected = mysql.createConnection({
-//     host: "localhost",
-//     user:"root",
-//     password: "",
-//     database: "db_restaurant_2024"
-// });
 const connected = mysql.createConnection({
-    host: "mysql-189700-0.cloudclusters.net",
-    port: "19666",
-    user: "admin",
-    password: "oah7MNJ2",
+    host: "localhost",
+    user:"root",
+    password: "",
     database: "db_restaurant_2024"
-})
+});
+// const connected = mysql.createConnection({
+//     host: "mysql-189700-0.cloudclusters.net",
+//     port: "19666",
+//     user: "admin",
+//     password: "oah7MNJ2",
+//     database: "db_restaurant_2024"
+// })
 connected.connect((err)=>{
     if(err) console.log(`Faild Connect Database`,err);
     console.log(`Connected Database`);
